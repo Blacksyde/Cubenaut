fc1fe7110f9f9e5e6c2578f86c8b52f528ca4a02
Blacksyde <alex.blacketor@gmail.com>
2018-09-25 17:50:09 -0500

    adding a little structure

---
+++ Assets/Scripts/Satellite.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Satellite : MonoBehaviour {
+
+	private int information;
+	private int money;
+	private int research;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
