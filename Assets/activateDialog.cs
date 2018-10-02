e25740bd6b213f3ce5bb2043ebcdb6b005815cf1
Li <tli67@illinois.edu>
2018-10-02 18:43:30 -0500

    dialog box

---
+++ Assets/activateDialog.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activateDialog : MonoBehaviour {
+
+public GameObject Dialog;
+
+void OnMouseDown () {
+
+	Debug.Log(Dialog.activeSelf);
+	Debug.Log(Dialog.activeInHierarchy);
+	Dialog.SetActive(true);
+	Debug.Log(Dialog.activeSelf);
+	Debug.Log(Dialog.activeInHierarchy);
+
+	}
+}
---
+++ Assets/planet_economy.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planet_economy : MonoBehaviour {
+	public int information;
+		
+	// Update is called once per frame
+	
+}
