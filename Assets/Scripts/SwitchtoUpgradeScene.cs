612ea5ca39c727aeaa2db9c04c227c3693263214
tanzhen li <tli67@illinois.edu>
2018-10-18 17:32:39 -0500

    upgrade scene initiated

---
+++ Assets/Scripts/SwitchtoUpgradeScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchtoUpgradeScene : MonoBehaviour {
+
+	public void SwitchUpgradeScene(){
+		SceneManager.LoadScene(1);
+	}
+
+	public void SwitchMainScene(){
+		SceneManager.LoadScene(0);
+	}
+
+}
