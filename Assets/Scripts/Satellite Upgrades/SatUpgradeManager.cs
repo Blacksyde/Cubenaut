8f6c3f4bebf83c126f96b5e684990a3aac5be0a3
tanzhen li <tli67@illinois.edu>
2018-10-30 18:00:19 -0500

    deleted lib

---
+++ Assets/Scripts/Satellite Upgrades/SatUpgradeManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SatUpgradeManager : MonoBehaviour {
+
+	public GameObject sat; 
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
